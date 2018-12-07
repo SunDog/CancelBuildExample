08759d75a3030e5da1eef0775a72706dddd84491
Carlos Filipe Rodrigues de Oliveira <cfsundog@gmail.com>
2018-12-07 10:20:09 +0100

    Initial Commit

---
+++ CancelBuildExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CancelBuildExample
+//
+//  Created by Carlos Deoliveira on 07.12.18.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
